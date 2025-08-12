e5436d8d232e28deb8aed093371dcebd785250cc
CodingLikeAMonkey <aaron.michael.friedrich@gmail.com>
2025-08-12 21:03:03 +0200

    Replacing godot-cpp with git module and printing NOT in editor

--- gdextension/src/test.cpp
+++ gdextension/src/test.cpp
@@ -1,7 +1,12 @@
 #include "test.h"
+#include <godot_cpp/classes/engine.hpp>
 #include <godot_cpp/variant/utility_functions.hpp>
 
 void Test::_ready() 
 {
+    if (Engine::get_singleton()->is_editor_hint()) 
+    {
+        return;
+    }
     UtilityFunctions::print("Test class is ready!");
 }
